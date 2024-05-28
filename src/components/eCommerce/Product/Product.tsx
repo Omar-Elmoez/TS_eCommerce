ce11a9d0c8b5ff4bc4cebb5005215aad7c56cae4
Omar Elmoez <omarelmoez2000@gmail.com>
2024-05-28 15:48:20 +0300

    disabled addToCart button for 300ms

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,17 +1,31 @@
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import styles from "./styles.module.css";
 import { IProduct } from "@/types";
 import { useAppDispatch } from "@/store/hooks";
 import { addToCart } from "@/store/cart/CartSlice";
+import { useEffect, useState } from "react";
 const { product, productImg } = styles;
 
 const Product = ({ id, title, price, img }: IProduct) => {
-
   const dispatch = useAppDispatch();
 
+  const [btnClickedNum, setBtnClickedNum] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    setIsDisabled(true);
+
+    const debounce = setTimeout(() => {
+      setIsDisabled(false);
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [btnClickedNum]);
+
   const addToCartHandler = () => {
-    dispatch(addToCart(id))
-  }
+    dispatch(addToCart(id));
+    setBtnClickedNum((prev) => prev + 1);
+  };
 
   return (
     <article className={product}>
@@ -21,8 +35,13 @@ const Product = ({ id, title, price, img }: IProduct) => {
       <section>
         <h2>{title}</h2>
         <h3>{price} EGP</h3>
-        <Button variant="info" style={{ color: "white" }} onClick={addToCartHandler}>
-          Add to cart
+        <Button
+          variant="info"
+          style={{ color: "white" }}
+          onClick={addToCartHandler}
+          disabled={isDisabled}
+        >
+          {isDisabled ? <><Spinner animation="border" size="sm" /> Loading....</> : "Add to cart"}
         </Button>
       </section>
     </article>

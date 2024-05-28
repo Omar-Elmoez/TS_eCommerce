5a0d3da0d007f36f9387407cd6e9da2fcf05a3d5
Omar Elmoez <omarelmoez2000@gmail.com>
2024-05-28 15:38:17 +0300

    add animation to cart icon

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,21 +1,35 @@
-import Logo from "@/assets/svg/cart.svg?react"
+import Logo from "@/assets/svg/cart.svg?react";
 
 import styles from "./styles.module.css";
 import { useAppSelector } from "@/store/hooks";
 import { getTotalQuantity } from "@/store/cart/selectors";
+import { useEffect, useState } from "react";
 
-const { basketContainer, basketQuantity } = styles;
+const { basketContainer, basketQuantity, basketpumping } = styles;
 
 const HeaderBasket = () => {
+  const [isAnimate, setIsAnimate] = useState(false);
 
-  const total = useAppSelector(getTotalQuantity)
-  
+  const total = useAppSelector(getTotalQuantity);
+
+  useEffect(() => {
+
+    if (!total) return;
+    setIsAnimate(true);
+
+    const bounce = setTimeout(() => {
+      setIsAnimate(false);
+    }, 300);
+
+    return () => clearTimeout(bounce);
+  }, [total]);
+
   return (
     <div className={basketContainer}>
       <Logo title="Cart icon" />
-      <div className={basketQuantity}>{total}</div>
+      <div className={`${basketQuantity} ${isAnimate ? basketpumping : ""}`}>{total}</div>
     </div>
   );
-}
+};
 
 export default HeaderBasket;
